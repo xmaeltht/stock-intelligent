1818e2a5d809986967f0085a0fb39407dfa234ed
Mael Kams <xmaeltht@gmail.com>
2026-07-17 11:48:14 -0400

    Restore paper portfolio router and model registration

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,7 @@
 from app.models.company import Company
+from app.models.paper_portfolio import PaperPortfolio, PaperTrade
 from app.models.stock_analysis import StockAnalysis
 from app.models.user import User
 from app.models.watchlist import WatchlistEntry
 
-__all__ = ["Company", "StockAnalysis", "User", "WatchlistEntry"]
+__all__ = ["Company", "PaperPortfolio", "PaperTrade", "StockAnalysis", "User", "WatchlistEntry"]
